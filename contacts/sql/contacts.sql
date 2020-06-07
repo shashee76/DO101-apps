d4d45eaec4d0b2f7d42af5a4e9ce1610b191cb6e
shashee76 <sheelesh.sharma@gmail.com>
2020-06-07 17:47:48 -0400

    we

--- contacts/sql/contacts.sql
+++ contacts/sql/contacts.sql
@@ -3,7 +3,7 @@ drop table if exists contacts;
 create table contacts(id serial primary key,firstname varchar(30) not null,lastname varchar(30) not null, email varchar(30) not null);
 
 insert into contacts(firstname, lastname, email) values
-  ('Bilbo','Baggins','bilbo@theshire.com'),
+  ('Parth','Sharma','parth@sstrade.com'),
   ('Frodo','Baggins','frodo@theshire.com'),
   ('Samwise','Gamgee','sam@theshire.com'),
   ('Peregrin','Took','pippin@theshire.com'),
